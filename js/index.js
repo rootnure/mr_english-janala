ed787d359925338a91c3deaf3bd45b972a537991
rootnure <nur.diu.2791@gmail.com>
2026-02-15 22:05:23 +0600

    Load level words and partially show on UI

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-const loadLessions = async () => {
+const lessonsLoadingSpiner = document.querySelector(
+    "#level-container>.loading",
+);
+const loadLessons = async () => {
+    lessonsLoadingSpiner.classList.remove("hidden");
     const url = "https://openapi.programming-hero.com/api/levels/all";
     const res = await fetch(url);
     const data = await res.json();
-    displayLessons(data.data);
+    showLessons(data.data);
 };
 
-const displayLessons = (lessons = []) => {
+const showLessons = (lessons = []) => {
     const lessonsContainer = document.getElementById("level-container");
     lessonsContainer.innerHTML = "";
     lessons.forEach((lesson) => {
@@ -13,10 +17,37 @@ const displayLessons = (lessons = []) => {
         const btn = document.createElement("button");
         btn.classList = "btn btn-outline btn-primary w-max text-nowrap";
         btn.setAttribute("title", lessonName);
-        btn.setAttribute("onclick", `loadLesson(${id})`);
+        btn.setAttribute("onclick", `loadLevelWords(${level_no})`);
         btn.textContent = "Level - " + level_no;
         lessonsContainer.appendChild(btn);
     });
+    lessonsLoadingSpiner.classList.add("hidden");
 };
 
-loadLessions();
+loadLessons();
+
+const loadLevelWords = async (level) => {
+    console.log(level);
+    const url = `https://openapi.programming-hero.com/api/level/${level}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showLevelWords(data.data);
+};
+const showLevelWords = (words = []) => {
+    const wordContainer = document.getElementById("word-container");
+    words.forEach((word) => {
+        // {id: 170, level: 6, word: 'Vicarious', meaning: 'পরোক্ষভাবে অনুভূতি গ্রহণ করা', pronunciation: 'ভিকেরিয়াস'}
+        const {
+            id,
+            level,
+            word: currentWord,
+            meaning,
+            pronounciation,
+        } = word || {};
+        const wordCard = document.createElement("div");
+        wordCard.innerHTML = `
+        <p>${currentWord}</p>
+        `;
+        wordContainer.appendChild(wordCard);
+    });
+};
